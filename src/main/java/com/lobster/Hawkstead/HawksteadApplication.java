0e6eabf0c7cc85a1157380a12fb1d464dd37aff8
mjsamaha <matthewsamaha@gmail.com>
2025-08-11 22:23:31 -0400

    initial commit - clean project

---
+++ src/main/java/com/lobster/Hawkstead/HawksteadApplication.java
@@ -0,0 +1,13 @@
+package com.lobster.Hawkstead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawksteadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawksteadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobster/Hawkstead/HawksteadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobster.Hawkstead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawksteadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
